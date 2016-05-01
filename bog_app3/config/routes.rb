5edc58cf950fefdc139db14d94ca3933d4bc1e21
seanvsville <seanvanho@gmail.com>
2016-04-30 19:29:22 -0700

    finished edit and update

--- bog_app3/config/routes.rb
+++ bog_app3/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root 'creatures#index'
 
-    #METHOD   #PATH                       #CONTROLLER#ACTION    #ALIAS
-    get       '/creatures'          =>    'creatures#index',    as: :creatures
-    post      '/creatures'          =>    'creatures#create'
-    get       '/creatures/new'      =>    'creatures#new',      as: :new_creature
-    get       '/creatures/:id'      =>    'creatures#show',      as: :creature
+    #METHOD   #PATH                         #CONTROLLER#ACTION    #ALIAS
+    get       '/creatures'            =>    'creatures#index',    as: :creatures
+    post      '/creatures'            =>    'creatures#create'
+    get       '/creatures/new'        =>    'creatures#new',      as: :new_creature
+    get       '/creatures/:id'        =>    'creatures#show',     as: :creature
+    get       '/creatures/:id/edit'   =>    'creatures#edit',     as: :edit_creature
+    patch     '/creatures/:id'        =>    'creatures#update'
 
 
 

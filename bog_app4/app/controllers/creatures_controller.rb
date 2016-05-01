2f288706f1ab8134a6736d372caab6ee9aaeb344
seanvsville <seanvanho@gmail.com>
2016-04-30 23:03:16 -0700

    finished create and show

--- bog_app4/app/controllers/creatures_controller.rb
+++ bog_app4/app/controllers/creatures_controller.rb
@@ -5,6 +5,26 @@ class CreaturesController < ApplicationController
     render :index
   end
 
+  def new
+    @creature = Creature.new
+    render :new
+  end
+
+  def create
+    Creature.create(creature_params)
+    redirect_to creatures_path
+  end
+
+  def show
+    @creature = Creature.find(params[:id])
+  end
+
+
+private
+
+def creature_params
+  params.required(:creature).permit(:name, :description)
+end
 
 
 end

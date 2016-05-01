59206112b4432ee4e93daa92cc07ed55d99e8ad9
seanvsville <seanvanho@gmail.com>
2016-05-01 09:35:09 -0700

    initial commit

---
+++ bog_app5/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  root 'creatures#index'
+
+  #METHOD   #PATH                     #CONTROLLER#ACTION    #ALIAS
+  get       '/creatures'        =>    'creatures#index',    as: :creatures
+
+
+end

5355fc172215bcfa3e12dc4b616b657a357de5ba
seanvsville <seanvanho@gmail.com>
2016-04-30 17:48:17 -0700

    finished w index and show

---
+++ bog_app2/app/controllers/creatures_controller.rb
@@ -0,0 +1,29 @@
+class CreaturesController < ApplicationController
+
+  def index
+    @creatures = Creature.all
+    render :index
+  end
+
+  def new
+    @creature = Creature.new
+    render :new
+  end
+
+  def create
+    Creature.create(creature_params)
+    redirect_to creatures_path
+  end
+
+  def show
+    @creature = Creature.find(params[:id])
+    render :show
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
+
+end
